bdd8da44620ec7713018279637bd61893877ec7e
Rishabh Verma <10rishabhv@gmail.com>
2023-03-23 14:15:15 +0530

    fix indentation

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 
 interface Todo {
-    task: string;
-    is_complete: boolean;
-    added_on: Date;
+  task: string;
+  is_complete: boolean;
+  added_on: Date;
 }
 
 @Component({
-    selector: 'app-todo-list',
-    templateUrl: './todo-list.component.html',
-    styleUrls: ['./todo-list.component.css']
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
 })
 
 export class TodoListComponent {
-    newTodo: string = '';
+  newTodo: string = '';
 
-    todos: Todo[] = [
-        { task: 'learn golang', is_complete: false, added_on: new Date() },
-        { task: 'study', is_complete: false, added_on: new Date() },
-        { task: 'learn golang', is_complete: false, added_on: new Date() },
-    ];
+  todos: Todo[] = [
+    { task: 'learn golang', is_complete: false, added_on: new Date() },
+    { task: 'study', is_complete: false, added_on: new Date() },
+    { task: 'learn golang', is_complete: false, added_on: new Date() },
+  ];
 
-    addTodo() {
-        if (this.newTodo.trim().length === 0) {
-            return;
-        }
-        this.todos.push({ task: this.newTodo, is_complete: false, added_on: new Date() });
-        this.newTodo = '';
+  addTodo() {
+    if (this.newTodo.trim().length === 0) {
+      return;
     }
+    this.todos.push({ task: this.newTodo, is_complete: false, added_on: new Date() });
+    this.newTodo = '';
+  }
 }
